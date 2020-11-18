27d3f8ff7f7a968c982d431ba13694cba948ccc6
= <=>
2020-11-18 15:07:05 -0300

    initial commit

---
+++ src/main/java/com/bootcamp/casadocodigo/CasadocodigoApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.casadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasadocodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasadocodigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/casadocodigo/CasadocodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadocodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
