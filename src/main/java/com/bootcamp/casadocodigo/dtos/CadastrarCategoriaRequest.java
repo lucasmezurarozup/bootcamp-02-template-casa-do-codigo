37a7af2fb55da4262907ea3dcdf3c47ccbf10ba9
Lucas Mezuraro <lucas.mezuraro@zup.com.br>
2020-11-20 12:23:23 -0300

    adicionando mais testes relacionados a insercao de uma categoria

--- src/main/java/com/bootcamp/casadocodigo/dtos/CadastrarCategoriaRequest.java
+++ src/main/java/com/bootcamp/casadocodigo/dtos/CadastrarCategoriaRequest.java
@@ -16,4 +16,8 @@ public class CadastrarCategoriaRequest {
     public String getNome() {
         return nome;
     }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- src/test/java/com/bootcamp/casadocodigo/controllers/CategoriaControllerTest.java
+++ src/test/java/com/bootcamp/casadocodigo/controllers/CategoriaControllerTest.java
@@ -3,6 +3,7 @@ package com.bootcamp.casadocodigo.controllers;
 import com.bootcamp.casadocodigo.dtos.CadastrarCategoriaRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -26,12 +27,63 @@ public class CategoriaControllerTest {
 
     private CadastrarCategoriaRequest cadastrarCategoriaRequest;
 
+    @BeforeEach
+    public void init() {
+        cadastrarCategoriaRequest = new CadastrarCategoriaRequest();
+        cadastrarCategoriaRequest.setNome("Web");
+    }
+
     @Test
     public void testandoInsercaoComCampoNomeNulo() throws Exception {
+        cadastrarCategoriaRequest.
+                setNome(null);
         mockMvc.perform(
                 post("/categoria/registrar")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(cadastrarCategoriaRequest)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testandoInsercaoComCampoNomeEmBranco() throws Exception {
+        cadastrarCategoriaRequest.
+                setNome("");
+        mockMvc.perform(
+                post("/categoria/registrar")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cadastrarCategoriaRequest)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testandoInsercaoComDadosCorretos() throws Exception {
+        cadastrarCategoriaRequest.
+                setNome("Web");
+        mockMvc.perform(
+                post("/categoria/registrar")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cadastrarCategoriaRequest)))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    public void testandoInsercaoComDuplicacaoNome() throws Exception {
+        cadastrarCategoriaRequest.
+                setNome("Java");
+        mockMvc.perform(
+                post("/categoria/registrar")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cadastrarCategoriaRequest)))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
+
+        mockMvc.perform(
+                post("/categoria/registrar")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cadastrarCategoriaRequest)))
                 .andDo(print())
                 .andExpect(status().isBadRequest());
     }
